5245a5da1a81d53ae1b719b5847589e045d169e6
rahulalbiorix <rahulg.albiorix@gmail.com>
2024-06-19 12:06:04 +0530

    deploy to firebase

---
+++ src/plugins/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB88b496aI4hNVqY1J_EWEnf6q56mxB9Eg',
+  authDomain: 'folder-structure-d0120.firebaseapp.com',
+  projectId: 'folder-structure-d0120',
+  storageBucket: 'folder-structure-d0120.appspot.com',
+  messagingSenderId: '231007320307',
+  appId: '1:231007320307:web:6e345d83ffa504db511f86'
+}
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+export default firebaseApp
