aff2533f63d0897340a399bd4c953a0d60c443e6
rahulalbiorix <rahulg.albiorix@gmail.com>
2024-06-19 12:37:39 +0530

    add firebase instance to main file

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import SvgIcon from '@jamescoyle/vue-icon'
+import firebaseApp from '@/plugins/firebase'
 
 const app = createApp(App)
 app.component('SvgIcon', SvgIcon)
 app.use(createPinia())
+app.use(firebaseApp)
 
 app.mount('#app')
